bb387070149bce87a555c60446727f7ca453922f
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 12:48:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,58 +2,44 @@ const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      let string = display.innerText.toString();
-      let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
-      if (operatorIndex == -1) {
-        display.innerText = "Invalid Input";
-      } else {
-        let operator = string[operatorIndex];
-        let operands = string.split(operator).map(parseFloat);
-        let result;
-        switch (operator) {
-          case "+":
-            result = operands.reduce((a, b) => a + b);
-            break;
-          case "-":
-            result = operands.reduce((a, b) => a - b);
-            break;
-          case "*":
-            result = operands.reduce((a, b) => a * b);
-            break;
-          case "/":
-            result = operands.reduce((a, b) => a / b);
-            break;
+    item.onclick = () => {
+        if (item.id == 'clear'){
+            display.innerText = '';
+        } else if (item.id == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length -1);
+        } else if (display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else if (item.id == "%") {
+            let string = display.innerText.toString();
+            let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
+            if (operatorIndex == -1) {
+                display.innerText = "Invalid Input";
+            } else {
+                let operand1 = string.substring(0, operatorIndex);
+                let operand2 = string.substring(operatorIndex+1);
+                let operator = string.charAt(operatorIndex);
+                let result = 0;
+                if (operator == "+") {
+                    result = parseFloat(operand1) + (parseFloat(operand1) * (parseFloat(operand2) / 100));
+                } else if (operator == "-") {
+                    result = parseFloat(operand1) - (parseFloat(operand1) * (parseFloat(operand2) / 100));
+                } else if (operator == "*") {
+                    result = parseFloat(operand1) * (parseFloat(operand2) / 100);
+                } else if (operator == "/") {
+                    result = parseFloat(operand1) / (parseFloat(operand2) / 100);
+                }
+                display.innerText = result;
+            }
+        } else{
+            display.innerText += item.id; 
         }
-        display.innerText = result.toString();
-      }
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else if (item.id == "%") {
-      let string = display.innerText.toString();
-      let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
-      if (operatorIndex == -1) {
-        display.innerText = "Invalid Input";
-      } else {
-        let operator = string[operatorIndex];
-        let operands = string.split(operator);
-        let number = parseFloat(operands[operands.length - 1]);
-        let percent = parseFloat(number) * 0.01;
-        let result = operands.slice(0, -1).join(operator) + operator + (number * percent).toString();
-        display.innerText = result;
-      }
-    } else {
-      display.innerText += item.id;
     }
-  };
-});
+})
+
 
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
