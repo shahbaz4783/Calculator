4b6a545bde8ac993a02fdf6bb1d08e57914da8d3
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 12:44:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,55 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
     item.onclick = () => {
-        if (item.id == 'clear'){
+        if (item.id == 'clear') {
             display.innerText = '';
-        } else if (item.id == 'backspace'){
+        } else if (item.id == 'backspace') {
             let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length -1);
-        } else if (display.innerText != '' && item.id == 'equal'){
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != '' && item.id == 'equal') {
             display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id == 'equal'){
+        } else if (display.innerText == '' && item.id == 'equal') {
             display.innerText = 'Empty!';
             setTimeout(() => (display.innerText = ''), 2000);
-        } else if (item.id == "%") {
-          let string = display.innerText.toString();
-          let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
-          if (operatorIndex == -1) {
-          display.innerText = "Invalid Input";
-       } else {
-          display.innerText += item.id; 
+        } else if (item.id == '%') {
+            let string = display.innerText.toString();
+            let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
+            if (operatorIndex == -1) {
+                display.innerText = parseFloat(string) / 100;
+            } else {
+                let operand1 = parseFloat(string.substr(0, operatorIndex));
+                let operator = string.substr(operatorIndex, 1);
+                let operand2 = parseFloat(string.substr(operatorIndex + 1));
+                if (isNaN(operand1) || isNaN(operand2)) {
+                    display.innerText = "Invalid Input";
+                } else {
+                    let result;
+                    switch (operator) {
+                        case "+":
+                            result = operand1 + (operand2 / 100 * operand1);
+                            break;
+                        case "-":
+                            result = operand1 - (operand2 / 100 * operand1);
+                            break;
+                        case "*":
+                            result = operand1 * (operand2 / 100);
+                            break;
+                        case "/":
+                            result = operand1 / (operand2 / 100);
+                            break;
+                        default:
+                            display.innerText = "Invalid Input";
+                            return;
+                    }
+                    display.innerText = result;
+                }
+            }
+        } else {
+            display.innerText += item.id;
         }
     }
-})
+});
+
 
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
