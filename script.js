9d96fc6fc73bb7021851e750b8d7a4eb9bc4dd0f
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 13:15:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ buttons.forEach((item) => {
             display.innerText = string.substr(0, string.length -1);
         } else if (display.innerText != '' && item.id == 'equal'){
             display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id == 'equal'){
+        } else if (item.id === "exponential") {
+          const operatorIndex = displayValue.lastIndexOf("+") || displayValue.lastIndexOf("-") || displayValue.lastIndexOf("*") || displayValue.lastIndexOf("/");
+          if (operatorIndex === -1) {
+          display.textContent = "Invalid Input";
+          return;
+        }
+
+          const firstNumber = parseFloat(displayValue.substring(0, operatorIndex));
+          const secondNumber = parseFloat(displayValue.substring(operatorIndex + 1, displayValue.length));
+          const result = Math.pow(firstNumber, secondNumber);
+
+          display.textContent = result;
+            
+         } else if (display.innerText == '' && item.id == 'equal'){
             display.innerText = 'Empty!';
             setTimeout(() => (display.innerText = ''), 2000);
         } else{
