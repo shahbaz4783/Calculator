8586685693c7fcc867f8a8ade321f52d8922d3b5
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 12:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
 buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else if (item.id == "%") {
-      let string = display.innerText.toString();
-      let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
-      if (operatorIndex == -1) {
-        display.innerText = "Invalid Input";
-      } else {
-        let operand = string.substr(operatorIndex + 1);
-        let percentage = parseFloat(operand) / 100;
-        let expression = string.substr(0, operatorIndex + 1) + percentage.toString();
-        display.innerText = eval(expression);
-      }
-    } else {
-      display.innerText += item.id;
+    item.onclick = () => {
+        if (item.id == 'clear'){
+            display.innerText = '';
+        } else if (item.id == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length -1);
+        } else if (display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else if (item.id == "%") {
+          let string = display.innerText.toString();
+          let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
+          if (operatorIndex == -1) {
+          display.innerText = "Invalid Input";
+        } else{
+          display.innerText += item.id; 
+        }
     }
-  };
-});
-
-
-
+})
 
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
