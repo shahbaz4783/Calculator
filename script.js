78c99ec5e54bfa0c236c708fd9ea24888743dc9d
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 12:50:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,43 +2,47 @@ const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
-    item.onclick = () => {
-        if (item.id == 'clear'){
-            display.innerText = '';
-        } else if (item.id == 'backspace'){
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length -1);
-        } else if (display.innerText != '' && item.id == 'equal'){
-            display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id == 'equal'){
-            display.innerText = 'Empty!';
-            setTimeout(() => (display.innerText = ''), 2000);
-        } else if (item.id == "%") {
-            let string = display.innerText.toString();
-            let operatorIndex = string.lastIndexOf("+") || string.lastIndexOf("-") || string.lastIndexOf("*") || string.lastIndexOf("/");
-            if (operatorIndex == -1) {
-                display.innerText = "Invalid Input";
-            } else {
-                let operand1 = string.substring(0, operatorIndex);
-                let operand2 = string.substring(operatorIndex+1);
-                let operator = string.charAt(operatorIndex);
-                let result = 0;
-                if (operator == "+") {
-                    result = parseFloat(operand1) + (parseFloat(operand1) * (parseFloat(operand2) / 100));
-                } else if (operator == "-") {
-                    result = parseFloat(operand1) - (parseFloat(operand1) * (parseFloat(operand2) / 100));
-                } else if (operator == "*") {
-                    result = parseFloat(operand1) * (parseFloat(operand2) / 100);
-                } else if (operator == "/") {
-                    result = parseFloat(operand1) / (parseFloat(operand2) / 100);
-                }
-                display.innerText = result;
-            }
-        } else{
-            display.innerText += item.id; 
-        }
+  item.onclick = () => {
+    if (item.id == 'clear') {
+      display.innerText = '';
+    } else if (item.id == 'backspace') {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != '' && item.id == 'equal') {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == '' && item.id == 'equal') {
+      display.innerText = 'Empty!';
+      setTimeout(() => (display.innerText = ''), 2000);
+    } else if (item.id == '%') {
+      let string = display.innerText.toString();
+      let operatorIndex = string.lastIndexOf('+') || string.lastIndexOf('-') || string.lastIndexOf('*') || string.lastIndexOf('/');
+      
+      // If the operatorIndex is -1, it means there is no operator in the string, so we show an error message.
+      if (operatorIndex == -1) {
+        display.innerText = 'Invalid Input';
+        return;
+      }
+      
+      let operand1 = string.substr(0, operatorIndex);
+      let operator = string.substr(operatorIndex, 1);
+      let operand2 = string.substr(operatorIndex + 1);
+      let result;
+      
+      // If the second operand is empty, we calculate the percentage of the first operand.
+      if (operand2 == '') {
+        result = parseFloat(operand1) / 100;
+      } else {
+        let expression = operand1 + operator + (parseFloat(operand1) * parseFloat(operand2) / 100);
+        result = eval(expression);
+      }
+      
+      // Update the display with the result.
+      display.innerText = result;
+    } else {
+      display.innerText += item.id;
     }
-})
+  };
+});
 
 
 
