8dc8f387782c67eb8099dbcfac49304f25313951
Shahbaz Ansari <96674638+shahbaz4783@users.noreply.github.com>
2023-04-23 13:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,23 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator__keys");
-const display = calculator.querySelector(".calculator__display");
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
 
-keys.addEventListener("click", (event) => {
-  if (!event.target.closest("button")) return;
-
-  const key = event.target;
-  const keyValue = key.textContent;
-  const displayValue = display.textContent;
-  const { type } = key.dataset;
-  const { previousKeyType } = calculator.dataset;
-
-  if (type === "number") {
-    if (displayValue === "0" || previousKeyType === "operator" || previousKeyType === "calculate") {
-      display.textContent = keyValue;
-    } else {
-      display.textContent = displayValue + keyValue;
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'clear'){
+            display.innerText = '';
+        } else if (item.id == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length -1);
+        } else if (display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else{
+            display.innerText += item.id; 
+        }
     }
-  }
-
-  if (type === "operator") {
-    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
-    operatorKeys.forEach((el) => el.dataset.state = "");
-    key.dataset.state = "selected";
-
-    calculator.dataset.firstNumber = displayValue;
-    calculator.dataset.operator = key.dataset.key;
-  }
-
-  if (type === "percent") {
-    const operatorIndex = displayValue.lastIndexOf("+") || displayValue.lastIndexOf("-") || displayValue.lastIndexOf("*") || displayValue.lastIndexOf("/");
-    if (operatorIndex === -1) {
-      display.textContent = "Invalid Input";
-      return;
-    }
-
-    const firstNumber = parseFloat(displayValue.substring(0, operatorIndex));
-    const percentValue = parseFloat(displayValue.substring(operatorIndex + 1, displayValue.length)) / 100;
-    const result = firstNumber * percentValue;
-
-    display.textContent = result;
-  }
-
-  if (type === "calculate") {
-    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
-    operatorKeys.forEach((el) => el.dataset.state = "");
-
-    const firstNumber = calculator.dataset.firstNumber;
-    const operator = calculator.dataset.operator;
-    const secondNumber = displayValue;
-
-    const result = calculate(firstNumber, operator, secondNumber);
-    display.textContent = result;
-
-    calculator.dataset.firstNumber = result;
-  }
-
-  calculator.dataset.previousKeyType = type;
-});
-
-function calculate(firstNumber, operator, secondNumber) {
-  firstNumber = parseFloat(firstNumber);
-  secondNumber = parseFloat(secondNumber);
-
-  if (operator === "plus") return firstNumber + secondNumber;
-  if (operator === "minus") return firstNumber - secondNumber;
-  if (operator === "times") return firstNumber * secondNumber;
-  if (operator === "divide") return firstNumber / secondNumber;
-}  
-
-
+})
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
 const calculator = document.querySelector('.calculator');
